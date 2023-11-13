9914e3161c85e26236ea9685e8580e35f4c69751
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-11-13 14:24:21 -0800

    added text and buttons

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,7 +1,7 @@
-from flask import ( Blueprint, render_template )
+from flask import ( Blueprint, render_template ) 
 
-bp = Blueprint('fact', __name__, url_prefix='/facts')
+bp = Blueprint('fact', __name__, url_prefix="/facts")
 
 @bp.route('/new')
-def new():
+def new(): 
     return render_template('facts/new.html')
